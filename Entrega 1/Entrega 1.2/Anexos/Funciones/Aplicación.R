fa90bcb0d5706b143f5892c74f0e496de1423f1d
Mendivenson <mbarraganz@unal.edu.co>
2024-08-04 09:07:11 -0500

    Finished 4 point in entrega 1.pdf

--- Entrega 1/Entrega 1.2/Anexos/Funciones/Aplicación.R
+++ Entrega 1/Entrega 1.2/Anexos/Funciones/Aplicación.R
@@ -34,6 +34,8 @@ for (i in tamaños){
 write.csv(resultados, file = 'Entrega 1/Entrega 1.2/Anexos/Resultados/ARL S Chart.csv', row.names = T)
 
 # Punto 3 -----------------------------------------------------------------
+
+# == ARL teórico
 source('Entrega 1/Entrega 1.2/Anexos/Funciones/Run Length X bar.R')
 tamaños = c(3,5,8,10)
 corrimientos = seq(0,3, by = 0.025)
@@ -46,3 +48,65 @@ for (i in tamaños){
 }
 
 write.csv(resultados, file = 'Entrega 1/Entrega 1.2/Anexos/Resultados/ARL X Chart.csv', row.names = T)
+
+# == ARL (20 subgrupos)
+# Simulación de los 20 subgrupos racionales
+mu = 0; sigma = 1                 # Parámetros para muestra aleatoria
+n = 3; m = 20                     # Tamaño de muestra y cantidad de muestras              
+set.seed(10)                      # Semilla para replicación
+muestras = matrix(nrow = m, ncol = n)
+for (i in 1:m){
+  muestras[i,] = rnorm(n, mean = mu, sd = sigma)
+}
+
+medias = rowMeans(muestras)               # Media de cada una de las muestras
+rangos = apply(muestras, MARGIN = 1,      # Rango de cada una de las muestras
+               FUN = function(x) diff(range(x)))
+rango = mean(rangos)                      # Media de los rangos
+media = mean(medias)                      # Media de las medias
+d2 = 1.693                                # Constante para hacer insesgado al rango.
+sigma = rango/d2
+
+# Al revisar el gráfico, no se encuentran medias afuera de los límites por lo que en fase II
+# se utilizan estas mismas estimaciones
+
+# Cálculo aproximado del ARL
+resultados = resultados["n = 3",]
+rownames(resultados) = 'Teórico'
+resultados['Fase I (20 subgrupos)',] = as.numeric(pbmclapply(corrimientos, 
+                                                             FUN = function(x) mean(RunLengthXMod_Optimized(mu1 = media, 
+                                                                                                            sigma1 = sigma,
+                                                                                                            Corrimiento = x,
+                                                                                                            m = 50000)),
+                                                             mc.cores = cores, mc.set.seed = 1305))
+
+# == ARL (50 subgrupos)
+# Simulación de los 50 subgrupos racionales
+mu = 0; sigma = 1                 # Parámetros para muestra aleatoria
+n = 3; m = 50                     # Tamaño de muestra y cantidad de muestras              
+set.seed(10)                      # Semilla para replicación
+muestras = matrix(nrow = m, ncol = n)
+for (i in 1:m){
+  muestras[i,] = rnorm(n, mean = mu, sd = sigma)
+}
+
+medias = rowMeans(muestras)               # Media de cada una de las muestras
+rangos = apply(muestras, MARGIN = 1,      # Rango de cada una de las muestras
+               FUN = function(x) diff(range(x)))
+rango = mean(rangos)                      # Media de los rangos
+media = mean(medias)                      # Media de las medias
+d2 = 1.693                                # Constante para hacer insesgado al rango.
+sigma = rango/d2
+
+# Al revisar el gráfico, no se encuentran medias afuera de los límites por lo que en fase II
+# se utilizan estas mismas estimaciones
+
+# Cálculo aproximado del ARL
+resultados['Fase I (50 subgrupos)',] = as.numeric(pbmclapply(corrimientos, 
+                                                             FUN = function(x) mean(RunLengthXMod_Optimized(mu1 = media, 
+                                                                                                            sigma1 = sigma,
+                                                                                                            Corrimiento = x,
+                                                                                                            m = 50000)),
+                                                             mc.cores = cores, mc.set.seed = 1305))
+
+write.csv(resultados, file = 'Entrega 1/Entrega 1.2/Anexos/Resultados/ARL X Chart theory and stimated.csv', row.names = T)
